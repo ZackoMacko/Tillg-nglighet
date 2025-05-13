fa15d4e5819544aa8b91849ccd5b9fbb548349f4
Isaac Nilsson <isaac.okrypterat@gmail.com>
2025-05-13 20:06:55 +0200

    Basic functionality
    +You can now buy books. One at a time.
    -The books needs to stored in a JSON-file for better functionality and optimization.
    -You can fill the input fields however the data they contained is not stored.

---
+++ script.js
@@ -0,0 +1,72 @@
+const books= document.getElementsByClassName("item");
+const total = document.getElementById("total");
+const availablebooks = Array.from(books);
+
+
+//const pendingBooks = document.getElementsByClassName("pendingpurchaseditem");
+//const pendingPurchasedBooks = Array.from(pendingBooks);
+var selectedBooks=[];
+
+
+
+document.addEventListener("DOMContentLoaded", async function () {
+
+    if(availablebooks.length>0){
+ 
+    localStorage.setItem("purchasedBook",availablebooks)
+    console.log(availablebooks);
+
+        availablebooks.forEach(element => {
+        element.addEventListener("click",() => {
+            selectedBooks = element;
+
+            const itemDivImage = element.querySelector("img.book")
+            //const image = itemDiv.querySelector("img.book")
+            const altText = itemDivImage.alt;
+            //console.log(altText)
+            //console.log(selectedBooks);
+            localStorage.setItem("selectedBook",altText);
+        });
+     
+    
+    });
+    }
+
+    const pendingPurchasedBook = localStorage.getItem("selectedBook")
+    //console.log(pendingPurchasedBook);
+
+    if(pendingPurchasedBook!=null){
+        //const  pendingPurchasedBookText = document.getElementsByClassName("pendingpurchaseditem").firstChild.innerHTML;
+        var pendingPurchasedBookText = document.getElementById("PPI0").firstElementChild.innerHTML;
+ 
+
+      
+        pendingPurchasedBookText = pendingPurchasedBook;
+       
+        document.getElementById("PPI0").firstElementChild.textContent = pendingPurchasedBookText;
+        total.lastElementChild.innerHTML = `<h2> ${150} Kr</h2>`;
+        console.log(pendingPurchasedBook.length);
+    }
+    /**
+     * const pendingPurchasedBooks = localStorage.getItem("selectedBooks")
+    if(pendingPurchasedBooks.length >0){
+        
+        const purchaseOverview = document.querySelectorAll(".pendingpurchaseditem")
+        console.log(purchaseOverview)
+        const bookTitles = pendingPurchasedBooks;
+        
+        
+
+        
+    //localStorage.setItem("pendingPurchasedBooks",pendingPurchasedBooks);
+    //Här vill jag koppla "pendingpurchaseditem till selectedBook"
+    
+    
+    
+    }
+     */
+    
+  
+
+   
+})
